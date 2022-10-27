35c5d32726f982c6c211a9373aac314aad8450ab
Carlos Camacho <ccamachog@avathartech.com>
2022-10-27 17:01:53 -0400

    Actualizando nueva versión del site del Barcamp 2022.

---
+++ js/main.js
@@ -0,0 +1,83 @@
+$(document).ready(function () {
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 600) {
+      $("header").addClass("on-scroll");
+      $("header .nav-item").removeClass("nav-item-secondary");
+      $("header .nav-item").addClass("nav-item-primary");
+      $("header .btn").removeClass("btn-solid-secondary");
+      $("header .btn").addClass("btn-solid-primary");
+    } else {
+      $("header").removeClass("on-scroll");
+      $("header .nav-item").addClass("nav-item-secondary");
+      $("header .nav-item").removeClass("nav-item-primary");
+      $("header .btn").addClass("btn-solid-secondary");
+      $("header .btn").removeClass("btn-solid-primary");
+    }
+  });
+
+  $('.sign-in').on('click', function () {
+    $('.sign-in-modal').removeClass('hidden');
+    $('body').addClass('modal-open');
+  });
+
+  $('.conduct-button').on('click', function () {
+    $('.conduct-modal').removeClass('hidden');
+    $('body').addClass('modal-open');
+  });
+
+  $('.speaker-card').on('click', function () {
+    $('.bio-modal').removeClass('hidden');
+    $('body').addClass('modal-open');
+  });
+
+  $('.close-modal').on('click', function () {
+    $('.modal').addClass('hidden');
+    $('body').removeClass('modal-open');
+  });
+
+  $('#openMenu').on('click', function () {
+    $('.main-menu').toggleClass('show');
+  });
+
+  $("header .nav-item").click(function(e) {
+    if (this.hash !== "") {
+      e.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - 60
+      }, 1000);
+    }
+  });
+
+  $("footer .nav-item").click(function(e) {
+    if (this.hash !== "") {
+      e.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - 60
+      }, 1000);
+    }
+  });
+
+  $('.event-handling a').on('click', function (e) {
+    let target = $(this).attr('id');
+    e.preventDefault();
+    $(this).addClass('active');
+    $('.event-handling a').not($(this)).removeClass('active');
+    $('.tabs-image-container').find(target).addClass('open');
+    $('.tabs-image-container .tab-content').not(target).removeClass('open');
+  });
+
+  var $filter = $('.tab-unconferences-container').isotope({
+    // options
+    itemSelector: '.uncoferences-card',
+    layoutMode: 'fitRows'
+  });
+
+  $('.tabs-handling').on( 'click', 'a', function() {
+    var filterValue = $( this ).attr('data-filter');
+    $filter.isotope({ filter: filterValue });
+  });
+});
